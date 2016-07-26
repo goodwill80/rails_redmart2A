4343cf91807e56eb096ca43d1ab5197a21544a04
kit jonathan ho <j.hokit80@gmail.com>
2016-07-26 21:19:08 +0800

    added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact_us'
+
   resources :categories_products
   resources :reviews
   resources :categories
   resources :products
   resources :users
-  root 'users#index'
+  root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
+
+  test "should get contact_us" do
+    get static_pages_contact_us_url
+    assert_response :success
+  end
+
+end
