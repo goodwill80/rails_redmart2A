e4ef9e66e8315f509cbe4d01dc5361b76d6d9f44
kit jonathan ho <j.hokit80@gmail.com>
2016-07-26 23:20:38 +0800

    amended

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/about'
+  get  '/users',  to: 'users#new'
+  root 'static_pages#home'
+  get  '/home',   to: 'static_pages#home'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact_us',   to: 'static_pages#contact_us'
 
-  get 'static_pages/contact_us'
-
+  resources :users
   resources :categories_products
   resources :reviews
   resources :categories
   resources :products
-  resources :users
-  root 'application#hello'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
